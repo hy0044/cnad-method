d6ae8062cb21bea169fb5d4086371f18f744c403
hy0044 <hy0044@gmail.com>
2026-09-14 06:49:54 +0900

    chore: add lint and formatting checks (#12)

--- test/update-failure.test.js
+++ test/update-failure.test.js
@@ -26,7 +26,17 @@ function git(cwd, ...args) {
 
 function commitAll(cwd) {
   git(cwd, 'add', '--all')
-  git(cwd, '-c', 'user.name=CNAD Test', '-c', 'user.email=cnad@example.invalid', 'commit', '--quiet', '-m', 'test fixture')
+  git(
+    cwd,
+    '-c',
+    'user.name=CNAD Test',
+    '-c',
+    'user.email=cnad@example.invalid',
+    'commit',
+    '--quiet',
+    '-m',
+    'test fixture',
+  )
 }
 
 function normalizedHash(content) {
